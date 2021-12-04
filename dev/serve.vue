c36636100749e04ff04a61e4137647591a7d4749
ajomuch92 <aaronmontes_92@yahoo.es>
2021-12-03 18:22:02 -0600

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueNumberInputFormatted from '@/vue-number-input-formatted.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueNumberInputFormatted
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-number-input-formatted />
+  </div>
+</template>
